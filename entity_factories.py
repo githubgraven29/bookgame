392c175b2d569fd03b2ac864d73ac064c07ed0ea
githubgraven29 <githubgraven29@gmail.com>
2021-07-13 02:35:53 -0400

    ridding code of the changes - didn't work anyway

--- entity_factories.py
+++ entity_factories.py
@@ -1,37 +1,34 @@
 from components.ai import HostileEnemy
 from components.consumable import HealingConsumable
-from components.fighter import Fighter, MonsterFighter
+from components.fighter import Fighter
+from components.inventory import Inventory
 from entity import Actor, Item
 
 
-
-
 player = Actor(
-    char="@", 
-    color=(255, 255, 255), 
-    name="Player", 
+    char="@",
+    color=(255, 255, 255),
+    name="Player",
     ai_cls=HostileEnemy,
     fighter=Fighter(hp=30, defense=2, power=5),
+    inventory=Inventory(capacity=26),
 )
-# what I want to do is adjust so that at creation, each orc has a chance to be different from the other orcs. 
-# I also want to change the attacks to become slightly randomized within each attack, so that each attack reps a dice roll.
-# The difference between the races will be the sides of the dice and the number of dice.
-# need to change orc code in some way to incorporate a random seed later such that each orc is unique.
 
 orc = Actor(
-    char="O", 
-    color=(63, 127, 63), 
-    name="Orc", 
+    char="o",
+    color=(63, 127, 63),
+    name="Orc",
     ai_cls=HostileEnemy,
-    fighter=MonsterFighter(low_hp = 8, high_hp = 12, min_power = 2, max_power = 3, min_defense = -1, max_defense = 1),
+    fighter=Fighter(hp=10, defense=0, power=3),
+    inventory=Inventory(capacity=0),
 )
-
 troll = Actor(
-    char="T", 
-    color=(0, 127, 0), 
-    name="Troll", 
+    char="T",
+    color=(0, 127, 0),
+    name="Troll",
     ai_cls=HostileEnemy,
-    fighter=MonsterFighter(low_hp = 13, high_hp = 19, min_power = 2, max_power = 6, min_defense = 0, max_defense = 2),
+    fighter=Fighter(hp=16, defense=1, power=4),
+    inventory=Inventory(capacity=0),
 )
 
 health_potion = Item(
